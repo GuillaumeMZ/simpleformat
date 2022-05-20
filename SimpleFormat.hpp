9adaa0ef07b52d0852915b4acf9505f1f14b2282
GuillaumeMZ <58004135+GuillaumeMZ@users.noreply.github.com>
2022-05-20 22:50:35 +0200

    Removed the class "Pattern"

--- SimpleFormat.hpp
+++ SimpleFormat.hpp
@@ -9,6 +9,8 @@ namespace format
 {
     namespace internal
     {
+        //DO NOT USE ANYTHING FROM THIS NAMESPACE DIRECTLY
+
         /************************************************************************/
         /*                              StringArgs                              */
         /************************************************************************/
@@ -28,8 +30,8 @@ namespace format
 
         template<std::size_t N>
         StringArgs<N>::StringArgs(std::array<std::string, N> args, std::size_t totalByteSize) :
-                _args(std::move(args)),
-                _totalByteSize(totalByteSize)
+                _args{std::move(args)},
+                _totalByteSize{totalByteSize}
         {
         }
 
@@ -45,7 +47,7 @@ namespace format
             return _args;
         }
 
-        template< std::size_t N, typename T, typename... Ts>
+        template<std::size_t N, typename T, typename... Ts>
         void add_next_string(std::array<std::string, N>& vec, std::size_t index, std::size_t& totalByteSize, const T& current, const Ts&... remaining)
         {
             vec[index] = std::to_string(current); //TODO change the way a value is converted to a string
@@ -66,77 +68,47 @@ namespace format
             return {std::move(result), totalByteSize};
         }
 
-        /************************************************************************/
-        /*                                Pattern                               */
-        /************************************************************************/
 
-        template<std::size_t N>
-        class Pattern
-        {
-            public:
-                Pattern(std::string pattern);
-                const std::string& getPattern() const;
-                const std::array<std::size_t, N>& getPlaceholders() const;
 
-            private:
-                std::string _pattern;
-                std::array<std::size_t, N> _placeholders;
-        };
+		template<std::size_t N>
+		auto make_placeholders(const std::string& source)
+		{
+			std::array<std::size_t, N> result{};
 
-        /************************************************************************/
-        /*                                Pattern                               */
-        /************************************************************************/
+			for(auto i = 0, j = 0; i < source.size(); ++i)
+			{
+				if(j >= N)
+					throw std::runtime_error("The number of arguments does not match the number of placeholders");
 
-        template<std::size_t N>
-        Pattern<N>::Pattern(std::string pattern) :
-                _pattern(std::move(pattern))
-        {
-            for(auto i = 0, j = 0; i < _pattern.size(); ++i)
-            {
-                if(j >= N)
-                    throw std::runtime_error("The number of arguments does not match the number of placeholders");
+				if(source[i] == '$')
+				{
+					result[j] = i;
+					++j;
+				}
+			}
 
-                if(_pattern[i] == '$')
-                {
-                    _placeholders[j] = i;
-                    ++j;
-                }
-            }
-        }
-
-        template<std::size_t N>
-        const std::string& Pattern<N>::getPattern() const
-        {
-            return _pattern;
-        }
-
-        template<std::size_t N>
-        const std::array<std::size_t, N>& Pattern<N>::getPlaceholders() const
-        {
-            return _placeholders;
-        }
+			return result;
+		}
 
         void overwrite_range(std::string &to, const std::string &from, std::size_t startTo, std::size_t startFrom, std::size_t endFrom);
 
         template<std::size_t N>
-        std::string fill_pattern(const Pattern<N>& pattern, const StringArgs<N>& args) //TODO: simplifier si possible
+        std::string fill_pattern(const std::string& pattern, const std::array<std::size_t, N>& placeholders, const StringArgs<N>& args) //TODO: simplifier si possible
         {
-            const auto& raw_pattern = pattern.getPattern();
-
             //Compute the total size of the string
-            auto final_size = raw_pattern.size() - pattern.getPlaceholders().size() + args.getTotalByteSize();
+            auto final_size = pattern.size() - placeholders.size() + args.getTotalByteSize();
             std::string result (final_size, '?'); //old way to initialize a string; bad >:(
 
             std::size_t globalOffset = 0;
             std::size_t lastNonReplacedIndex = 0;
 
-            for(auto i = 0; i < pattern.getPlaceholders().size(); ++i)
+            for(auto i = 0; i < placeholders.size(); ++i)
             {
                 const std::string& currentString = args.toArray()[i];
-                const std::size_t currentPlaceholder = pattern.getPlaceholders()[i] + globalOffset - i;
+                const std::size_t currentPlaceholder = placeholders[i] + globalOffset - i;
 
                 //Filling the non-placeholder part
-                overwrite_range(result, raw_pattern, lastNonReplacedIndex, lastNonReplacedIndex - globalOffset + i, currentPlaceholder - globalOffset + i);
+                overwrite_range(result, pattern, lastNonReplacedIndex, lastNonReplacedIndex - globalOffset + i, currentPlaceholder - globalOffset + i);
                 lastNonReplacedIndex = currentPlaceholder + currentString.size();
 
                 //Filling the placeholder part
@@ -146,7 +118,7 @@ namespace format
 
             return result;
         }
-    }
+    } //end of namespace format::internal
 
     template<typename... Ts>
     std::string format(std::string source, const Ts&... args)
@@ -156,15 +128,15 @@ namespace format
         if constexpr(N == 0)
             return source;
 
-        internal::Pattern<N> pattern{std::move(source)};
+        const auto& placeholders = internal::make_placeholders<N>(source);
         const auto& stringArgs = internal::make_stringargs<N>(args...);
 
-        return internal::fill_pattern(pattern, stringArgs);
+        return internal::fill_pattern(source, placeholders, stringArgs);
     }
 
     template<typename... Ts>
-    void print(const Ts&... args)
+    void print(std::string source, const Ts&... args)
     {
-        std::fputs(format(args...).c_str(), stdout); //because puts adds a newline whereas fputs doesn't
+        std::fputs(format(std::move(source), args...).c_str(), stdout); //because puts adds a newline whereas fputs doesn't
     }
-}
+} //end of namespace format
